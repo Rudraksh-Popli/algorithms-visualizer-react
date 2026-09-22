04b856408728fb0b0c2e111478217beef2fe82f5
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-09-22 10:22:21 +0530

    Fix GitHub Pages build path

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "client",
   base: "/algorithms-visualizer-react/",
   plugins: [react()],
   server: {
     port: 5173
   },
   build: {
-    outDir: "client/dist"
+    outDir: "dist",
+    emptyOutDir: true
   }
 });
