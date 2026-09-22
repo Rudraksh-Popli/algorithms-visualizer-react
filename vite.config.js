71fd0b7e21d8d080400a5c0b666d1b6448598c58
Rudraksh-Popli <poplirudraksh00@gmail.com>
2026-09-22 10:17:49 +0530

    added GitHub pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/algorithms-visualizer-react/",
   plugins: [react()],
   server: {
     port: 5173
+  },
+  build: {
+    outDir: "client/dist"
   }
 });
